18602a38d045de70cc6cc92053dfe3f29403832a
Maria <mdpogorelova@gmail.com>
2025-06-08 12:32:42 +1200

    feature: implemented model (without 10 runs). All features are included. Accuracy 0.96

---
+++ models/decision_tree_kdd.py
@@ -0,0 +1,45 @@
+import time
+
+import findspark
+findspark.init()
+
+from pyspark import SparkFiles
+
+from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer, VectorAssembler, OneHotEncoder
+from pyspark.ml.classification import DecisionTreeClassifier
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+
+spark = SparkSession.builder.appName("Decision Tree Model").getOrCreate()
+
+spark.sparkContext.addFile("dataset.csv")
+
+df = spark.read.csv(SparkFiles.get("dataset.csv"), header=False, inferSchema=True)
+
+# Step 1: Convert label (string) to numeric
+label_indexer = StringIndexer(inputCol="_c41", outputCol="label")
+data = label_indexer.fit(df).transform(df)
+
+# Step 2: Assemble vector from features
+num_features = 40
+assembler = VectorAssembler(inputCols=[f"_c{i}"for i in range(num_features)], outputCol="features")
+data = assembler.transform(data)
+
+# Step 3: Split data into train and test data
+train_data, test_data = data.randomSplit([0.7, 0.3], seed=42)
+
+# Step 4: Create the classifier
+dt_classifier = DecisionTreeClassifier(labelCol="label", featuresCol="features")
+
+# Step 5: train the model
+model = dt_classifier.fit(train_data)
+
+# Step 6: test the model
+predictions = model.transform(test_data)
+
+evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
+accuracy = evaluator.evaluate(predictions)
+
+print(f"Test Accuracy: {accuracy:.2f}")
